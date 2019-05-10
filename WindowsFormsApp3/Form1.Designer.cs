da6e656d641d63689e7bd5def9a6f9944094101e
Master Aleksandr <hsanya2000@mail.ru>
2019-05-10 11:01:47 +0500

    Privet

---
+++ WindowsFormsApp3/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+namespace WindowsFormsApp3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+        
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pl = new System.Windows.Forms.PictureBox();
+            this.fireball = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pl)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.fireball)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pl
+            // 
+            this.pl.BackColor = System.Drawing.Color.Transparent;
+            this.pl.Image = global::WindowsFormsApp3.Properties.Resources.down_stend;
+            this.pl.Location = new System.Drawing.Point(139, 179);
+            this.pl.Name = "pl";
+            this.pl.Size = new System.Drawing.Size(40, 77);
+            this.pl.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pl.TabIndex = 0;
+            this.pl.TabStop = false;
+            this.pl.Click += new System.EventHandler(this.pl_Click);
+            // 
+            // fireball
+            // 
+            this.fireball.BackColor = System.Drawing.Color.Transparent;
+            this.fireball.Image = global::WindowsFormsApp3.Properties.Resources.a0cd84b264f852e4c7af031efb08af91_1_1_;
+            this.fireball.Location = new System.Drawing.Point(220, 179);
+            this.fireball.Name = "fireball";
+            this.fireball.Size = new System.Drawing.Size(42, 43);
+            this.fireball.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.fireball.TabIndex = 1;
+            this.fireball.TabStop = false;
+            this.fireball.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::WindowsFormsApp3.Properties.Resources.map;
+            this.ClientSize = new System.Drawing.Size(1344, 690);
+            this.Controls.Add(this.fireball);
+            this.Controls.Add(this.pl);
+            this.Cursor = System.Windows.Forms.Cursors.Cross;
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pl)).EndI  nit();
+            ((System.ComponentModel.ISupportInitialize)(this.fireball)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pl;
+        private PictureBox fireball;
+    }
+}
+
---
+++ WindowsFormsApp3/Form1.cs
@@ -0,0 +1,263 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace WindowsFormsApp3
+{
+    public partial class Form1 : Form
+    {
+        int stepChange = 1;
+        public Form1()
+        {
+            InitializeComponent();
+            this.AllowTransparency = true;
+            pl.BackColor = Color.Transparent;
+            this.MouseClick += new MouseEventHandler(Form1MouseDown);
+            this.KeyDown += new KeyEventHandler(OKP);
+
+        }
+
+        string lastButton = "S";
+        private void OKP(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode.ToString())
+            {
+                case "X":
+                    switch(lastButton)
+                    {
+                        case "A":
+                            fireball.Size = new Size(44, 44);
+                            fireball.Image = Properties.Resources.a0cd84b264f852e4c7af031efb08af91_1_1_;
+                            fireball.Location = new Point (pl.Location.X - 40, pl.Location.Y);
+                            break;
+                        case "D":
+                            var x = pl.Location.X;
+                            while(x < 1285)
+                            {
+                                x += 1;
+                                fireball.Size = new Size(44, 44);
+                                fireball.Image = Properties.Resources.a0cd84b264f852e4c7af031efb08af91_1_1_;
+                                fireball.Location = new Point(x, pl.Location.Y);
+                            }
+                            break;
+                        case "S":
+                            fireball.Size = new Size(44, 44);
+                            fireball.Image = Properties.Resources.a0cd84b264f852e4c7af031efb08af91_1_1_;
+                            fireball.Location = new Point(pl.Location.X, pl.Location.Y + 65);
+                            break;
+                        case "W":
+                            fireball.Size = new Size(44, 44);
+                            fireball.Image = Properties.Resources.a0cd84b264f852e4c7af031efb08af91_1_1_;
+                            fireball.Location = new Point(pl.Location.X, pl.Location.Y - 40);
+                            break;
+                    }
+                    break;
+                case "A":
+                    if (pl.Location.X > 10)
+                    {
+                        if (stepChange==1)
+                        {
+                            
+                            stepChange = 2;
+                            pl.Size = new Size(40, 65);
+                            pl.Image = Properties.Resources.left_step1;
+                            pl.Location = new Point(pl.Location.X - 7, pl.Location.Y+1);
+                            
+                        }
+                        else if(stepChange == 2)
+                        {
+                            stepChange = 3;
+                            pl.Size = new Size(40, 65);
+                            pl.Image = Properties.Resources.left_stend;
+                            pl.Location = new Point(pl.Location.X -7, pl.Location.Y);
+                            
+                        }
+                        else if (stepChange == 3)
+                        {
+                            stepChange = 4;
+                            pl.Size = new Size(40, 65);
+                            pl.Image = Properties.Resources.left_step2;
+                            pl.Location = new Point(pl.Location.X - 7, pl.Location.Y - 1);
+                        }
+                        else 
+                        {
+                            stepChange = 1;
+                            pl.Size = new Size(40, 65);
+                            pl.Image = Properties.Resources.left_stend;
+                            pl.Location = new Point(pl.Location.X - 7, pl.Location.Y );
+
+                        }
+                    }
+                    lastButton = "A";  
+                    break;
+
+                case "D":
+
+                    if (pl.Location.X < 1285)
+                    {
+                        if (stepChange == 1)
+                        {
+                            stepChange = 2;
+                            pl.Size = new Size(40, 65);
+                            pl.Image = Properties.Resources.right_step1;
+                            pl.Location = new Point(pl.Location.X + 7, pl.Location.Y + 1);
+
+                        }
+                        else if (stepChange == 2)
+                        {
+                            stepChange = 3;
+                            pl.Size = new Size(40, 65);
+                            pl.Image = Properties.Resources.right_stend;
+                            pl.Location = new Point(pl.Location.X + 7, pl.Location.Y);
+
+                        }
+                        else if (stepChange == 3)
+                        {
+                            stepChange = 4;
+                            pl.Size = new Size(40, 65);
+                            pl.Image = Properties.Resources.right_step2;
+                            pl.Location = new Point(pl.Location.X + 7, pl.Location.Y - 1);
+                        }
+                        else
+                        {
+                            stepChange = 1;
+                            pl.Size = new Size(40, 65);
+                            pl.Image = Properties.Resources.right_stend;
+                            pl.Location = new Point(pl.Location.X + 7, pl.Location.Y);
+
+                        }
+                    }
+                    lastButton = "D";
+                    break;
+
+                case "W":
+                    if (pl.Location.Y >44)
+                    {
+                        if (stepChange == 1)
+                        {
+                            stepChange = 2;
+                            pl.Size = new Size(45, 65);
+                            pl.Image = Properties.Resources.up_step1;
+                            pl.Location = new Point(pl.Location.X, pl.Location.Y - 7);
+
+                        }
+                        else if (stepChange == 2)
+                        {
+                            stepChange = 3;
+                            pl.Size = new Size(45, 65);
+                            pl.Image = Properties.Resources.up_stend;
+                            pl.Location = new Point(pl.Location.X, pl.Location.Y - 7);
+
+                        }
+                        else if (stepChange == 3)
+                        {
+                            stepChange = 4;
+                            pl.Size = new Size(45, 65);
+                            pl.Image = Properties.Resources.up_step2;
+                            pl.Location = new Point(pl.Location.X, pl.Location.Y - 7);
+
+                        }
+                        else if (stepChange == 4)
+                        {
+                            stepChange = 1;
+                            pl.Size = new Size(45, 65);
+                            pl.Image = Properties.Resources.up_stend;
+                            pl.Location = new Point(pl.Location.X, pl.Location.Y - 7);
+
+                        }
+                    }
+                    lastButton = "W";
+                    break;
+                case "S":
+                    
+                    if (pl.Location.Y < 586)
+                    {
+                        if (stepChange==1)
+                        {
+                            stepChange = 2;
+                            pl.Size = new Size(45, 65);
+                            pl.Image = Properties.Resources.down_step1;
+                            pl.Location = new Point(pl.Location.X, pl.Location.Y + 7);
+
+                        }
+                        else if (stepChange == 2)
+                        {
+                            stepChange = 3;
+                            pl.Size = new Size(45, 65);
+                            pl.Image = Properties.Resources.down_stend;
+                            pl.Location = new Point(pl.Location.X, pl.Location.Y + 7);
+
+                        }
+                        else if (stepChange == 3)
+                        {
+                            stepChange = 4;
+                            pl.Size = new Size(45, 65);
+                            pl.Image = Properties.Resources.down_step2;
+                            pl.Location = new Point(pl.Location.X, pl.Location.Y + 7);
+
+                        }
+                        else if (stepChange == 4)
+                        {
+                            stepChange = 1;
+                            pl.Size = new Size(45, 65);
+                            pl.Image = Properties.Resources.down_stend;
+                            pl.Location = new Point(pl.Location.X, pl.Location.Y + 7);
+
+                        }
+                    }
+                    lastButton = "S";
+                    break;
+                default:
+                    pl.Image = Properties.Resources.up_stend;
+                    break;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+        
+
+        void Form1MouseDown(object sender, MouseEventArgs e)
+        {
+            switch (e.Button)
+            {
+                case MouseButtons.Left:
+                    fireball.Size = new Size(44, 44);
+                    fireball.Image = Properties.Resources.a0cd84b264f852e4c7af031efb08af91_1_1_;
+                    fireball.Location = pl.Location;
+
+                    for (int x = pl.Location.X; x <= e.Location.X; x++)
+                    {
+                        fireball.Size = new Size(44, 44);
+                        fireball.Image = Properties.Resources.a0cd84b264f852e4c7af031efb08af91_1_1_;
+                        fireball.Location = new Point(fireball.Location.X + x, fireball.Location.Y);
+                    }
+                    break;
+                default:
+                    break;
+
+                
+            }
+
+        }
+
+        private void pl_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
